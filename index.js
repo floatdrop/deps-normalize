0619c83b6f9f7a1467ade03673c320c54a6c64e9
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-08-21 14:21:04 +0600

    feat(normalize): moved code from bem-object

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-module.exports = function () {
+var parseBemId = require('parse-bem-identifier');
 
+module.exports = function (deps) {
+    var res = [];
+
+    if (typeof deps === 'string') { deps = parseBemId(deps, { short: true }); }
+
+    if (Object.keys(deps).length === 0) {
+        throw new Error(this.block + ' have empty deps object');
+    }
+
+    if (typeof deps.elems === 'string') { deps.elems = [ deps.elems ]; }
+
+    if (deps.elem !== undefined && deps.elems !== undefined) {
+        throw new Error('Cannot have `elem` and `elems` in its dependencies');
+    }
+
+    if (deps.mod !== undefined && deps.mods !== undefined) {
+        throw new Error('Cannot have `mod` and `mods` in dependencies');
+    }
+
+    if (deps.elems) {
+        deps.elems.forEach(function(elem) {
+            res.push({ elem: elem });
+        });
+    }
+
+    if (deps.mods) {
+        Object.keys(deps.mods).forEach(function(mod) {
+            if (typeof deps.mods[mod] === 'string') {
+                deps.mods[mod] = [deps.mods[mod]];
+            }
+            deps.mods[mod].forEach(function(value) {
+                res.push({ mod: mod, value: value });
+            });
+        });
+    }
+
+    if (!deps.elems && !deps.mods) {
+        res.push(deps);
+    }
+
+    return res;
 };
--- test/index.js
+++ test/index.js
@@ -1,7 +1,86 @@
 /* global describe, it */
 
-var normalize = require('..');
+var nomralize = require('../');
+require('should');
 
-it('should normalize string', function (done) {
-    done('Not yet implemented');
+describe('BEMobject.expand', function () {
+    it('should throw expection on empty object', function () {
+        (function () {
+            nomralize({});
+        }).should.throw();
+    });
+
+    it('should properly handle string', function () {
+        nomralize('block').should.eql([{
+            block: 'block'
+        }]);
+
+        nomralize('block__elem').should.eql([{
+            block: 'block',
+            elem: 'elem'
+        }]);
+
+        nomralize('block__elem_mod').should.eql([{
+            block: 'block',
+            elem: 'elem',
+            mod: 'mod'
+        }]);
+
+        nomralize('block__elem_mod_val').should.eql([{
+            block: 'block',
+            elem: 'elem',
+            mod: 'mod',
+            value: 'val'
+        }]);
+
+        nomralize('block_mod').should.eql([{
+            block: 'block',
+            mod: 'mod',
+        }]);
+        nomralize('block_mod_val').should.eql([{
+            block: 'block',
+            mod: 'mod',
+            value: 'val'
+        }]);
+    });
+
+    it('should properly handle single elem', function () {
+        nomralize({ elem: 'singleElem' }).should.eql([{
+            elem: 'singleElem'
+        }]);
+    });
+
+    it('should properly handle multiple elements', function () {
+        nomralize({ elems: ['row', 'cell'] }).should.eql([
+            { elem: 'row' },
+            { elem: 'cell' }
+        ]);
+    });
+
+    it('should properly handle single mod with value', function () {
+        nomralize({ mod: 'color', value: 'white' }).should.eql([{
+            mod: 'color', value: 'white'
+        }]);
+    });
+
+    it('should properly handle multiple mods with single values', function () {
+        nomralize({ mods: { color: 'white', position: 'top' }}).should.eql([
+            { mod: 'color', value: 'white' },
+            { mod: 'position', value: 'top' }
+        ]);
+    });
+
+    it('should properly handle multiple mods with multiple values', function () {
+        nomralize({ mods: { color: 'white', position: ['top', 'bottom'] }}).should.eql([
+            { mod: 'color', value: 'white' },
+            { mod: 'position', value: 'top' },
+            { mod: 'position', value: 'bottom' }
+        ]);
+    });
+
+    it('should throw exception when both `elem` and `elems` defined (also `mod` and `mods`)', function () {
+        (function() { nomralize({ elem: '', elems: [] }); }).should.throw();
+        (function() { nomralize({ mod: '', mods: [] }); }).should.throw();
+    });
+
 });
